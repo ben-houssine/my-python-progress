efdedde01496719e891c267e1d15c9d9ed9ba8b5
Youssef <87530308+ben-houssine@users.noreply.github.com>
2022-09-18 15:15:55 +0200

    Add files via upload

---
+++ Dictionarys.py
@@ -0,0 +1,23 @@
+
+# dictionarys {key:value}
+
+week_plan = {'Monday': ['working: from 9am - 1pm', 'coding at home', 'eating dinner with friends'],
+             'Tuesday': 'going to the gym',
+             'Wednesday': 'studying math and coding',
+             'Thursday': 'working from 10am - 11pm',
+             'Friday': 'going to the gym',
+             'Saturday': 'studying math and coding',
+             'Sunday': {'morning': 'working from 10am - 11pm', 'afternoon': 'eating dinner with friends'}   # a dict inside a dict is possible.
+              }
+
+print(week_plan)
+
+
+"""
+Outputs:
+
+{'Monday': ['working: from 9am - 1pm', 'coding at home', 'eating dinner with friends'], 'Tuesday': 'going to the gym', 'Wednesday': 'studying math and coding', 'Thursday': 'working from 10am - 11pm', 'Friday': 'going to the gym', 'Saturday': 'studying math and coding', 'Sunday': {'morning': 'working from 10am - 11pm', 'afternoon': 'eating dinner with friends'}}
+
+
+
+"""
