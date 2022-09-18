e5b82f0282cb0ca56f1567d6e9c1ed2c6e350357
Youssef <87530308+ben-houssine@users.noreply.github.com>
2022-09-18 12:49:59 +0200

    Update zip.py

--- zip.py
+++ zip.py
@@ -1,26 +1,22 @@
 
 #creating two arrays
-numbers_1 = [1,2,3]
-names_1 = ['name1','name2','name3']
+numbers = [1,2,3]
+names = ['name1','name2','name3']
 
 #one way to display items of an array, one by one together:
-displaying_option_1 = list(zip(numbers_1, names_1))
+displaying_option_1 = list(zip(numbers, names))
 print(displaying_option_1)
 print("")
 
 #another way to display it:
-#creating two arrays
-numbers_2 = [1,2,3]
-names_2= ['name1','name2','name3']
-
-for val1, val2 in zip(numbers_2, names_2):
+for val1, val2 in zip(numbers, names):
     print(val1,val2)
 
 """
-Option one will have the following output:
+1st output:
 [(1, 'name1'), (2, 'name2'), (3, 'name3')]
 
-Option two will have this output:
+2nd output:
 1 name1
 2 name2
 3 name3
