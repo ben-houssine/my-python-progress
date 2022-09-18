ea2d3cecfbaa705cbf2c03da03fa9e167b9bfed3
Youssef <87530308+ben-houssine@users.noreply.github.com>
2022-09-18 13:59:47 +0200

    Add files via upload

---
+++ enumerate_function.py
@@ -0,0 +1,13 @@
+
+#enumerates
+buying_list = ["apples", "potatoes","water","tomatoes"]
+for i in enumerate(buying_list):
+    print(i)
+
+    """
+outputs:    
+(0, 'apples')
+(1, 'potatoes')
+(2, 'water')   
+(3, 'tomatoes')
+    """
