b9c6008532296adf0f47ee065039e1de8ec38833
Youssef <87530308+ben-houssine@users.noreply.github.com>
2022-09-22 19:46:10 +0200

    Add files via upload
    For loop exercises

---
+++ task_4.py
@@ -0,0 +1,18 @@
+
+
+a,b = 10, 21
+sum_of_a_b = a+b
+
+
+if (sum_of_a_b % 2) == 0:
+   print("c ist eine gerade Zahl.")
+else:
+   print("c ist eine ungerade Zahl.")
+
+sum_of_a_b -=1
+print(sum_of_a_b)
+
+if (sum_of_a_b % 2) == 0:
+   print("c ist eine gerade Zahl.")
+else:
+   print("c ist eine ungerade Zahl.")
