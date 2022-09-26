41ef25ae9e2ee5be648db4301c03d443c41ee463
Youssef <87530308+ben-houssine@users.noreply.github.com>
2022-09-26 17:04:42 +0200

    Update classes_and_objects.py

--- classes_and_objects.py
+++ classes_and_objects.py
@@ -21,3 +21,11 @@ print('Car2: {}, {} Kg' .format(car2.type, car2.weight))
 
 car1.addWeight(15) #adds extra weight to the standard value contained in 'class car'
 print("Car1: {}" .format(vars(car1)))
+
+#Outputs:
+#A new car has ben created
+#A new car has ben created
+
+#Car1: BMW 520i Limousine, 1790 Kg
+#Car2: RS 6 Avant, 2150 Kg
+#Car1: {'type': 'BMW 520i Limousine', 'weight': 1805, 'age': 2014, 'car_aviable': 'yes'}
